159f5d9ab8e9915f35633e681b87c87deae09124
Simon <martwyandroid@gmail.com>
2025-01-13 16:09:41 +0100

    Dodanie klasy gracza

--- main.py
+++ main.py
@@ -3,12 +3,20 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable  = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -20,7 +28,18 @@ def main():
             
         screen.fill("black")
 
+        
+        #player.update(dt)
+        for obj in updatable:
+            obj.update(dt)
+
+        #player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
